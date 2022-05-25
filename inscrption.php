9d07e5150845917c79c584d9e599b963739d0fa0
togobouk@gmail.com <togobouk@gmail.com>
2022-05-25 17:16:26 +0000

    first commit

---
+++ inscrption.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <form action="inscription.php" method="post">
+        <div class="v141_110">
+            <div class="v141_111">
+
+            </div>
+            <span class="v141_112">Age :</span>
+            <span class="v141_113">Email :</span>
+            <span class="v141_114">Téléphone :</span>
+            <span class="v141_115">Prénoms :</span>
+            <span class="v141_116">Nom :</span>
+            <span class="v141_117">Catégorie:</span>
+            <span class="v141_118"> 
+
+            </span>
+            <div class="v141_119"></div>
+          <input type="text" name="nom" class="v141_120">
+            <div class="v141_121"></div>
+            <input type="text" name="mail" class="v141_122">
+            <input type="text" name="telephone" class="v141_123">
+            <div class="v141_124"></div>
+            <div class="name"></div>
+            <input type="text" name="prenom" class="v141_126">
+            <div class="v141_127"></div>
+            <span class="v141_128">RETOUR</span>
+            <span class="v141_129">INSCRIPTION</span>
+            <div class="v141_130"></div>
+            <input type="submit" value="Envoyer"  class="v141_131" >
+        </div>
+    </form>
+    </body>
+    </html>
